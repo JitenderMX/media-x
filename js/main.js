1fade78b5ba8fe380f31fd9be245368a84b41630
JitenderMX <jitender.work.mediax@gmail.com>
2024-05-27 12:04:41 +0530

    service and Sub service page

--- js/main.js
+++ js/main.js
@@ -9,7 +9,38 @@ AOS.init();
 
 
 
-
+$(".casp-slider").slick({
+  dots: false,
+  arrows: false,
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  responsive: [
+      {
+          breakpoint: 992,
+          settings: {
+              slidesToShow: 4,
+              slidesToScroll: 1,
+          }
+      },
+      {
+          breakpoint: 768,
+          settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1
+          }
+      },
+      {
+          breakpoint: 576,
+          settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+          }
+      }
+  ]
+})
 
 
 
